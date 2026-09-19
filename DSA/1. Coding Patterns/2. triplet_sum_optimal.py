810f5ff81f8979338cff88e32938ef8cefbc5742
memphis95 <goelankit1995@gmail.com>
2026-09-19 21:48:06 +0530

    triplet sum optimal solution

---
+++ DSA/1. Coding Patterns/2. triplet_sum_optimal.py
@@ -0,0 +1,64 @@
+from typing import List
+
+def pair_sum_sorted_all_pairs(nums: List[int], start: int, target: int) -> List[int]:
+    pairs =[]
+    left, right = start, len(nums)-1
+
+    while left < right:
+        sum = nums[left] + nums[right]
+        if sum == target:
+            pairs.append([nums[left], nums[right]])
+            left += 1
+            # avoid duplicate '[b, c]' pairs, skip 'b' if it's the same as
+            # previous number
+            while left < right and nums[left] == nums[left-1]:
+                left += 1
+        elif sum < target:
+            left += 1
+        else:
+            right -= 1
+    return pairs 
+
+
+
+
+def triplet_sum(nums: List[int]) -> List[List[int]]:
+    triplets = []
+    nums.sort()
+    for i in range(len(nums)):
+        # triplet consisting of only positive numbers will never sum to 0
+        if nums[i] > 0:
+            break
+        # avoid duplicate triplets, skip 'a', if it's the same as the previous
+        # number
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+
+        pairs = pair_sum_sorted_all_pairs(nums, i+1, -nums[i])
+        for pair in pairs:
+            triplets.append([nums[i]] + pair)
+    return triplets
+
+"""
+    Complexity Analysis
+        Time Complexity  : O(n2) 
+                            sort the array -> O(nlogn)
+                            for each element of the nums,
+                                run pair_sum_sorted_all_pairs( 0(n)) -> O(n2)
+                            O(nlogn) + O(n2) -> O(n2)
+        Space Complexity : O(n) 
+                            space taken up by the python sorting algorithm
+
+"""
+
+"""
+    Test Cases:
+        1. an empty array
+        2. a single-element array
+        3. a two element array
+        4. an array where all three of its values are the same
+        5. an array with no triplets that sum to 0
+        6. an array with duolicate triplets
+    
+
+"""
